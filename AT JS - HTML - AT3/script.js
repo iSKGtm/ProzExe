2f9be4672deff1aa504a11dd7413882661b373ad
Anthony Max <maximosenamaximo@gmail.com>
2023-10-03 10:54:18 -0300

    att

--- AT JS - HTML - AT3/script.js
+++ AT JS - HTML - AT3/script.js
@@ -1,13 +1,21 @@
-function connect() {
-    let titulo = document.querySelector("h1");
-    titulo.innerText = "Venda de um produto"
+let titulo = document.createElement("h1");
 
-    let elementoJS = document.createElement("li");
-    elementoJS.innerText = "JS"
-    elementoJS.id = "ling-js"
+titulo.id = "titulo";
+titulo.innerText = "Loja de bebidas";
 
-    let listaLinguagens = document.querySelector(".lista-linguagens");
-    listaLinguagens.appendChild(elementoJS);
+let body = document.querySelector("body");
+
+body.appendChild(titulo);
+
+let produto = document.createElement("div");
 
 
-}
+produto.innerHTML = `
+  <div>
+    <h2>Whisky Jack Daniels 150 Anos/h2>
+    <p>1 Litro</p>
+    <p id="valor">R$ 599.99</p>
+  </div>
+`;
+
+body.appendChild(produto)
