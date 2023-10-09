844b5c711d780e7413ba137a23ed42df905b5d64
Anthony Max <maximosenamaximo@gmail.com>
2023-10-09 20:17:08 -0300

    att

---
+++ AULA JS - HTML - CSS (09.10)/script.js
@@ -0,0 +1,11 @@
+const login = document.querySelector('h2');
+login.style.color = 'red';
+login.style.fontSize = '20px';
+
+const button = document.getElementById('buttonLog');
+button.style.color = 'blue';
+button.style.border = '2px solid blue';
+button.style.borderRadius = '13px';
+button.style.backgroundColor = 'white';
+
+/* Anthony Max, Alano Silva, Luan G Dantas, FELIPE RIBEIRO */
