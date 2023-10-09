6e97e9f00ae32ceec9c951858f04656836253196
Anthony Max <maximosenamaximo@gmail.com>
2023-10-09 20:48:20 -0300

    att

--- AULA JS - HTML - CSS (09.10)/script.js
+++ AULA JS - HTML - CSS (09.10)/script.js
@@ -1,8 +1,10 @@
 const login = document.querySelector('h2');
+login.style.textAlign = 'center';
 login.style.color = 'red';
 login.style.fontSize = '20px';
 
 const button = document.getElementById('buttonLog');
+button.style.alignSelf = 'center';
 button.style.color = 'blue';
 button.style.border = '2px solid blue';
 button.style.borderRadius = '13px';
